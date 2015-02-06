6284aa859f68b5a140dfe51cedec4c60b5c61bda
River Marmorstein <river@rmarmorstein.com>
2015-02-05 23:25:05 -0500

    Add intake to maven, and started to add the configs and main classes.

--- src/main/java/com/rmarmorstein/general/General.java
+++ src/main/java/com/rmarmorstein/general/General.java
@@ -1,7 +1,23 @@
 package com.rmarmorstein.general;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by River on 2/5/2015.
  */
-public class General {
+public class General extends JavaPlugin {
+    
+    private ModLoader modLoader = null;
+    
+    @Override
+    public void onEnable() {
+        modLoader = new ModLoader(this);
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+        
+    }
 }
---
+++ src/main/java/com/rmarmorstein/general/ModLoader.java
@@ -0,0 +1,16 @@
+package com.rmarmorstein.general;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Created by River on 2/5/2015.
+ * Handles loading different modules associated with the plugin*
+ */
+
+public class ModLoader {
+    
+    public ModLoader(Plugin plugin) {
+        
+        
+    }
+}
